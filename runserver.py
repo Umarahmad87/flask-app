7be105f5b3a9187d77dd040cae1005c9c63546b6
Umarahmad87 <umarahmadsiddiqi@outlook.com>
2021-03-07 23:10:51 +0500

    make it better

--- runserver.py
+++ runserver.py
@@ -7,16 +7,6 @@ from AIGrader import app
 from flask import Flask, request
 
 app ==Flask(__name__);
-"""
-en-core-web-sm
-pywin32
-pywinpty==0.5.7
-mkl-fft
-mkl-random
-mkl-service
-setuptools==52.0.0.post20210125
-tensorboard-plugin-wit==1.6.0
-"""
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
